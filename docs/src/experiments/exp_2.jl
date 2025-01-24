3877fd77dff8d68d1d8c6637b3a92062cb7561b7
Alex's mac <a.croscarrillo@gmail.com>
2025-01-24 21:24:47 +0100

    fixity fix

--- docs/src/experiments/exp_2.jl
+++ docs/src/experiments/exp_2.jl
@@ -1,14 +1,18 @@
 # # [Animations](@id exp)
-Animations are a great way of conveying science which extend well beyond the capabilities of standard static papers. 
+# Animations are a great way of conveying science which extend well beyond the capabilities of standard static papers. 
 
-# ### An interesting animation
-# Yes, animations are supported and are super easy to do!
-
+# ### A super simple animation
+# Watch how easy they are to implement! As before, check all the details of this page in its source code at `/docs/src/experiments/exp_2.jl`.
 using Pkg
 Pkg.instantiate()
 
 using Plots, LaTeXStrings
 
+@gif for n=1:100
+    heatmap(randn(100,100))
+end
+
+# ### An interesting animation
 # Function to generate Wiener process paths
 function geometric_brownian_motion(T, N, μ, σ, S_0; N_processes=1)
     dt = T / N  # Time step size
@@ -27,7 +31,3 @@ t, S = geometric_brownian_motion(T, N, μ, σ, S_0, N_processes = N_processes)
 @gif for i in 1:N+1
     plot(t[1:i], S[1:i, :], xlabel=L"t", ylabel=L"S(t)", title="Geometric Brownian Motion Simulation", legend=false, lw=2, grid=true, xlims=(0, T), ylims=(minimum(S), maximum(S)))
 end
-
-# ### A rather puzzling heatmap
-# This is a heatmap of a random normal matrix.
-heatmap(randn(100,100))
