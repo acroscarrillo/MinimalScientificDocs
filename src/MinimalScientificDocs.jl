e5f301a093b5581b6a0411620c8ae5bf36a7ab3d
Alex's mac <a.croscarrillo@gmail.com>
2025-01-24 13:44:45 +0100

    first commit

---
+++ src/MinimalScientificDocs.jl
@@ -0,0 +1,8 @@
+# src/Test_Module.jl
+module MinimalScientificDocs
+
+export mul_by_2
+
+include("aux.jl")
+
+end # end of module
---
+++ src/aux.jl
@@ -0,0 +1,9 @@
+# src/aux.jl
+""" 
+    mul_by_2(x)
+
+Multiplies x by 2.
+"""
+function mul_by_2(x) 
+    return 2*x 
+end
