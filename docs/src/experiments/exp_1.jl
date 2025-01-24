e5375c6c2acb4e4791f54957670aa8a4bd6a59ef
Alex's mac <a.croscarrillo@gmail.com>
2025-01-24 16:34:48 +0100

    minor fix 2

--- docs/src/experiments/exp_1.jl
+++ docs/src/experiments/exp_1.jl
@@ -1,14 +1,13 @@
 # # [Plot and Histogram](@id exp)
 # The following page has been generated entirely within a Julia script, see its source code to see how easy it was done. As a show case, here's some random plots using in this case `Plots.jl`.
 
-# ### An interesting plot
+# ### An puzzling plot
 using Pkg
 Pkg.instantiate()
 
 using Plots
 
 plot(rand(100,2))
-
 
 
 # ### A rather boring normal
