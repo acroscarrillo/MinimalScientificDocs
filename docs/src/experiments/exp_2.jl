f5149822d9e6e743a1cade96e0253b001942c0eb
Alex's mac <a.croscarrillo@gmail.com>
2025-01-24 21:16:43 +0100

    fix

--- docs/src/experiments/exp_2.jl
+++ docs/src/experiments/exp_2.jl
@@ -1,7 +1,7 @@
 # # [Animations](@id exp)
 
 
-# ### An interesting scatter
+# ### An interesting animation
 # Yes sir, animations are supported too!
 
 using Pkg
@@ -15,12 +15,13 @@ function geometric_brownian_motion(T, N, μ, σ, S_0; N_processes=1)
     t = range(0, T, length=N+1)  # Time points
     dW = sqrt(dt) .* randn(N, N_processes)  # Wiener increments
     W = cumsum([zeros(1, N_processes); dW], dims=1)  # Wiener process paths
-    S = S0 .* exp.((mu - 0.5 * σ^2) .* t .+ σ .* W)
+    S = S_0 .* exp.((μ - 0.5 * σ^2) .* t .+ σ .* W)
     return t, S
 end
 
 # Generate GBM data
-t, S = geometric_brownian_motion(10, 1000, 0.05, 0.2, 100, 10)
+T, N, μ, σ, S_0, N_processes = 10, 100, 0.15, 0.2, 100, 10
+t, S = geometric_brownian_motion(T, N, μ, σ, S_0, N_processes = N_processes)
 
 # Create animation
 @gif for i in 1:N+1
